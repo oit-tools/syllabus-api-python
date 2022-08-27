6c5cfee4a3c4fcc54422d7db7313a8f30e5aae02
kota <52403688+yashikota@users.noreply.github.com>
2022-08-27 20:39:59 +0900

    update: jsonの取得先をwebに変更

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import json
 import uvicorn
 from fastapi import FastAPI
+import requests
 
-
-with open("./2022.json") as f:
-    data = json.load(f)
+url = "https://raw.githubusercontent.com/a-kaibu/syllabus-scraping/master/2022.json?token=GHSAT0AAAAAABT4CTWQN5O46JGYAPCV6XBUYYKADNQ"
+session = requests.Session()
+data = session.get(url)
+json_data = json.loads(data.text)
 
 app = FastAPI()
 
 @app.get("/{numbering}")
 async def get_syllabus(numbering: str):
-    return data[numbering]
+    return json_data[numbering]
 
 def main():
     uvicorn.run(app, host="localhost", port=8000, reload=True)
