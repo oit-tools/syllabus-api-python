4232e16cf865b7cab760ab843b68ef5546a33f63
kota <52403688+yashikota@users.noreply.github.com>
2022-08-29 17:39:35 +0900

    del: requests

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import json
 import uvicorn
 from fastapi import FastAPI
-import requests
+import urllib.request
 
 url = "https://raw.githubusercontent.com/oit-tools/syllabus-scraping/master/2022.json"
-session = requests.Session()
-data = session.get(url)
-json_data = json.loads(data.text)
+session = urllib.request.urlopen(url)
+data = json.loads(session.read())
 
 app = FastAPI()
 
 @app.get("/{numbering}")
 async def get_syllabus(numbering: str):
-    return json_data[numbering]
+    return data[numbering]
 
 def main():
     uvicorn.run(app, host="localhost", port=8000, reload=True)
